96dc0a1fb52e8278ed25fc43fb7796902f5d0047
RicoWilliams <ricowilliams@protonmail.com>
2022-03-25 09:29:57 -0600

    indexscreen compnent bioler added, src and screens folders added

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,13 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>Index Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
