3eb11a7ac6fb5df965ae9840a0744c38e3b4f086
RicoWilliams <ricowilliams@protonmail.com>
2022-03-26 09:42:10 -0600

    inital styling added to createscreen.js

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,15 +1,38 @@
-import React, { useContext } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { Text, TextInput, View, StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext';
 
 const CreateScreen = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
   return (
     <View>
-      <Text>Create Screen</Text>
+      <Text style={styles.label}>Enter Title:</Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+      />
+      <Text style={styles.label}>Enter Content:</Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        onChangeText={(text) => setContent(text)}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: 'black'
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 10
+  }
+});
 
 export default CreateScreen;
